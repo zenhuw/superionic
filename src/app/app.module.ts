490adc0c7d9410e21cb3eb7a45205a4dd3d3ba19
Reza Aulia <zenhuwmank@gmail.com>
2017-08-08 10:34:35 +0700

    variouserr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,6 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-<<<<<<< HEAD
-
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-=======
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
@@ -15,17 +10,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import {ExpandableComponent} from '../components/expandable/expandable';
->>>>>>> 1166c332c76e4a8528db2475b1abd0c52745ad15
 
 @NgModule({
   declarations: [
     MyApp,
-<<<<<<< HEAD
-    HomePage
-=======
     HomePage,
     ExpandableComponent
->>>>>>> 1166c332c76e4a8528db2475b1abd0c52745ad15
   ],
   imports: [
     BrowserModule,
